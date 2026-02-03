45341f517dfd855d6c7439494738aed6afbd2c6c
RedBlackAka <140876408+RedBlackAka@users.noreply.github.com>
2026-02-03 10:36:35 +0100

    Test 15

--- src/citra_qt/citra_qt.cpp
+++ src/citra_qt/citra_qt.cpp
@@ -4008,13 +4008,10 @@ void GMainWindow::LoadTranslation() {
     if (UISettings::values.language.isEmpty()) {
         // Use the system's default locale
         const auto languages =
-            QLocale::system().uiLanguages();
+            QLocale::system().uiLanguages(QLocale::TagSeparator::Underscore);
 
         for (const auto& lang : languages) {
             loaded = translator.load(lang, {}, {}, QStringLiteral(":/languages/"));
-            if (loaded)
-                UISettings::values.language = lang;
-                break;
         }
     } else {
         // Otherwise load from the specified file
