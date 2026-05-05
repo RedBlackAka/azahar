8ec44cc534c9656a88852e3ae29b981ce1975272
RedBlackAka <140876408+RedBlackAka@users.noreply.github.com>
2026-05-05 02:35:37 +0200

    Test buffers

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -33,7 +33,7 @@ using namespace Common::Literals;
 using namespace Pica::Shader::Generator;
 
 constexpr u64 STREAM_BUFFER_SIZE = 64_MiB;
-constexpr u64 UNIFORM_BUFFER_SIZE = 8_MiB;
+constexpr u64 UNIFORM_BUFFER_SIZE = 16_MiB;
 constexpr u64 TEXTURE_BUFFER_SIZE = 2_MiB;
 
 constexpr vk::BufferUsageFlags BUFFER_USAGE =
